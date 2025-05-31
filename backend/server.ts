58cc7d1ae5532f62b4580a4bfc6aec5f51da2e60
Sofia Ortega <s.ortega@tamu.edu>
2025-05-31 14:14:24 -0500

    basic chat app

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,6 @@
 import express, { Request, Response } from "express";
 import { createServer } from "node:http";
-// import { Server } from "socket.io";
+import { Server } from "socket.io";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 
@@ -8,19 +8,26 @@ const PORT = 3000;
 
 const app = express();
 const server = createServer(app);
-// const io = new Server(server);
+const io = new Server(server, {});
 
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req: Request, res: Response) => {
   // res.send("hello world!!! ");
   res.sendFile(join(__dirname, "index.html"));
 });
 
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
 server.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
-
-// io.on("connection", (socket) => {
-//   console.log("a user connected");
-// });
